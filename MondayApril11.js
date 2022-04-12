e399aeaefe1a62d6522fdfedccba9d58d927e228
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-04-11 19:15:35 -0500

    Add files via upload

---
+++ MondayApril11.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function callAge(birthYear) {
+   const age = 2037 - birthYear;
+
+   function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.long(output);
+   
+   if (birthYear >= 2010 && birthYear <= 2010) { const str = `Oh, and you're a millenial, ${firstName}`;
+   console.log(str);
+   }
+   console.log(str);
+} 
+printAge();
+
+   return age;
+}
+
+const firstName = 'Yasmine';
+calcAge(2010);
+// console.log(age);
+// printAge()
